5bb0b9b21fa64f48fc51030220d36b5c8919fee6
PANKYAAA <pankajs.gaikar@gmail.com>
2020-07-30 01:16:00 +0530

    Destini challenge - Demo app for flutter. Inspired by London App Brewery course from Udemy.

---
+++ android/app/src/main/kotlin/com/tricksmachine/destinichallenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tricksmachine.destinichallenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storyTitle;
+  String choiceOne;
+  String choiceTwo;
+
+  Story({String storyTitle, String choice1, String choice2}) {
+    this.storyTitle = storyTitle;
+    this.choiceOne = choice1;
+    this.choiceTwo = choice2;
+  }
+}
